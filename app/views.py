41a69574a409b4d8f5de997ec88e37fb24383aaf
Anirudh Ramnath <anirudh@Anirudhs-MacBook-Pro.local>
2015-11-08 12:18:01 -0600

    dev

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from app import app
 from flask import render_template
+from flask import request
 import os
 
 PATH_TO_DATA = os.getcwd()+'/app/static/data'
@@ -17,4 +18,10 @@ def index():
 
 @app.route('/vitals/')
 def vitals():
-    return render_template('index.html')
+    return render_template('chooseOptions.html')
+   # return render_template('index.html')
+
+@app.route('/showVitals', methods=['POST'])
+def showVitals():
+    body_system = request.form.getlist("bodySystem")
+    return render_template('index.html', body_system = body_system)

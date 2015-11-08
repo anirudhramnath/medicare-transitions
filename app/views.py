5bfbbb26a3071e9f56adc616b319981e4a9eecc0
Ubuntu <ubuntu@ip-172-31-41-76.us-west-2.compute.internal>
2015-11-08 00:14:19 +0000

    dev

--- app/views.py
+++ app/views.py
@@ -1,7 +1,20 @@
 from app import app
 from flask import render_template
+import os
 
+PATH_TO_DATA = os.getcwd()+'/app/static/data'
 
 @app.route('/')
 def index():
-        return render_template('index.html', fromPython='changed')
+        patient_image = []
+
+        patients = os.listdir(PATH_TO_DATA)
+        for patient in patients:
+            patient_image.append( ('static/data/'+patient+'/image.png', patient) )
+
+        print patient_image
+        return render_template('choosePatient.html', patient_image=patient_image)
+
+@app.route('/vitals/')
+def vitals():
+    return render_template('index.html')

276382ca5666f3bad1b226928a6b559fc6d0ad81
Gowdhaman Sadhasivam <gsadha2@uic.edu>
2015-11-22 20:45:44 -0600

    Patients bs filters and others

--- app/views.py
+++ app/views.py
@@ -1,9 +1,11 @@
 from app import app
 from flask import render_template
 from flask import request
+from flask import Flask, session
 import os
 
 PATH_TO_DATA = os.getcwd()+'/app/static/data'
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 @app.route('/')
 def index():
@@ -20,10 +22,23 @@ def index():
 def vitals():
 	if request.method == 'POST':
 		print request.form['alignment']
-	return render_template('chooseOptions.html')
+		plist = request.form['alignment']
+		plist = plist.split(',')
+	patient_image = []
+	for patient in plist:
+		patient_image.append(('../'+patient))
+	return render_template('chooseOptions.html', patient_image=patient_image)
    # return render_template('index.html')
 
 @app.route('/showVitals', methods=['POST'])
 def showVitals():
-    body_system = request.form.getlist("bodySystem")
-    return render_template('index.html', body_system = body_system)
+	body_system = request.form.getlist("bodySystem")
+	if body_system == []:
+		body_system = session['tmp']
+		vitals_list = []
+		vitals_list = request.form.getlist("vitals")
+	else:
+		session['tmp'] = body_system
+		vitals_list = ['1','2','3','4','5','6']
+	return render_template('index.html', body_system = body_system,vitals_list = vitals_list)
+
